696d512e0a19fe8f6b1a35b7bbcd598bb532088c
Raghu Anand <raghuaanand@gmail.com>
2023-11-07 12:06:05 +0530

    Add files via upload
    initial commit

---
+++ js/questions.js
@@ -0,0 +1,172 @@
+// let questions = [
+//     {
+//     numb: 1,
+//     question: "What does HTML stand for?",
+//     answer: "Hyper Text Markup Language",
+//     options: [
+//       "Hyper Text Preprocessor",
+//       "Hyper Text Markup Language",
+//       "Hyper Text Multiple Language",
+//       "Hyper Tool Multi Language"
+//     ]
+//   },
+//     {
+//     numb: 2,
+//     question: "What does CSS stand for?",
+//     answer: "Cascading Style Sheet",
+//     options: [
+//       "Common Style Sheet",
+//       "Colorful Style Sheet",
+//       "Computer Style Sheet",
+//       "Cascading Style Sheet"
+//     ]
+//   },
+//     {
+//     numb: 3,
+//     question: "What does PHP stand for?",
+//     answer: "Hypertext Preprocessor",
+//     options: [
+//       "Hypertext Preprocessor",
+//       "Hypertext Programming",
+//       "Hypertext Preprogramming",
+//       "Hometext Preprocessor"
+//     ]
+//   },
+//     {
+//     numb: 4,
+//     question: "What does SQL stand for?",
+//     answer: "Structured Query Language",
+//     options: [
+//       "Stylish Question Language",
+//       "Stylesheet Query Language",
+//       "Statement Question Language",
+//       "Structured Query Language"
+//     ]
+//   },
+//     {
+//     numb: 5,
+//     question: "What does XML stand for?",
+//     answer: "eXtensible Markup Language",
+//     options: [
+//       "eXtensible Markup Language",
+//       "eXecutable Multiple Language",
+//       "eXTra Multi-Program Language",
+//       "eXamine Multiple Language"
+//     ]
+//   },
+// ];
+
+
+
+let questions = [
+  {
+    numb: 1,
+    question: "Who is the current Prime Minister of India?",
+    answer: "Narendra Modi",
+    options: [
+      "Rahul Gandhi",
+      "Amit Shah",
+      "Narendra Modi",
+      "Manmohan Singh"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the name of the Indian spacecraft that successfully landed on the Moon's south pole in 2021?",
+    answer: "Chandrayaan-2",
+    options: [
+      "Mangalyaan",
+      "Chandrayaan-2",
+      "Astrosat",
+      "Aditya-L1"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which state in India was the first to implement the Goods and Services Tax (GST) in 2017?",
+    answer: "Haryana",
+    options: [
+      "Karnataka",
+      "Haryana",
+      "Gujarat",
+      "Maharashtra"
+    ]
+  },
+  {
+    numb: 4,
+    question: "In 2021, which Indian cricketer became the fastest to reach 8,000 runs in One Day Internationals (ODIs)?",
+    answer: "Virat Kohli",
+    options: [
+      "Rohit Sharma",
+      "Shikhar Dhawan",
+      "Virat Kohli",
+      "MS Dhoni"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Who is the Chief Justice of India as of 2023?",
+    answer: "N.V. Ramana",
+    options: [
+      "Sharad Arvind Bobde",
+      "Ranjan Gogoi",
+      "N.V. Ramana",
+      "T.S. Thakur"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which state in India is known as the 'Land of Five Rivers'?",
+    answer: "Punjab",
+    options: [
+      "Haryana",
+      "Rajasthan",
+      "Punjab",
+      "Uttar Pradesh"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In 2022, India hosted the G20 Summit. Which city was the host for this event?",
+    answer: "New Delhi",
+    options: [
+      "Mumbai",
+      "Bangalore",
+      "Chennai",
+      "New Delhi"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who is the President of India as of 2023?",
+    answer: "Droupadi Murmu",
+    options: [
+      "Pranab Mukherjee",
+      "Ram Nath Kovind",
+      "A.P.J. Abdul Kalam",
+      "Droupadi Murmu"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which Indian state is known as the 'Spice Garden of India'?",
+    answer: "Kerala",
+    options: [
+      "Tamil Nadu",
+      "Karnataka",
+      "Andhra Pradesh",
+      "Kerala"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the name of the world's tallest statue, located in the Indian state of Gujarat?",
+    answer: "Statue of Unity",
+    options: [
+      "Statue of Liberty",
+      "Statue of Peace",
+      "Statue of Unity",
+      "Statue of Brotherhood"
+    ]
+  }
+];
